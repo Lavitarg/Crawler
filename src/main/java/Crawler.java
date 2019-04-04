cd5d2f465312204b4c7dc5224a90eaf8f5eecadf
Lavitarg <sermarsel@yandex.ru>
2019-04-04 14:24:35 +0300

    Added page rank algorhytm

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class Crawler {
@@ -18,7 +19,7 @@ public class Crawler {
     //обьявляем переменную-счетчик строк, и итоговый лист ссылок
     static Integer counterOfStrings = new Integer(1);
     int numberofFiles;
-    ArrayList<String> finalListofLinks = new ArrayList(numberofFiles);
+    public ArrayList<String> finalListofLinks = new ArrayList(numberofFiles);
 
     public void doCrawl(String URL) throws IOException {
 
@@ -52,5 +53,66 @@ public class Crawler {
         }
         writer.flush();
         writer.close();
+    }
+
+    public HashMap<String,Double> getPRMap()  throws IOException {
+        ArrayList<String> pages = finalListofLinks;
+        HashMap<String,ArrayList<String>> linksToOtherP= new HashMap<String, ArrayList<String>>();
+
+        for(String page:pages){
+
+            ArrayList<String> outerLinks = new ArrayList<String>();
+            //получаем страницу
+            Document currentPage= Jsoup.connect(page).get();
+            //вытаскиваем все ссылки
+            Elements links = currentPage.select("a");
+            for(Element link:links){
+                if(!outerLinks.contains(link.attr("href"))&&!link.attr("href").contains("#")&& !link.attr("href").contains(".jpg")){
+                    outerLinks.add(link.attr("href"));
+                }
+            }
+
+            linksToOtherP.put(page,outerLinks);
+        }
+        System.out.println("Links prepared");
+
+        return calculatePageRanks(linksToOtherP);
+
+    }
+
+    private HashMap calculatePageRanks (HashMap<String,ArrayList<String>> linksMap){
+        //creating of outputMap
+        HashMap<String,Double> resultMap = new HashMap<String, Double>();
+        //setting initial values
+        for(String link:linksMap.keySet()){
+            resultMap.put(link,0.15);
+        }
+        for(int i=0;i<1000;i++) {
+            //идем по всем ключам итоговой мапы
+            for (String pageURL : resultMap.keySet()) {
+                //для каждого идем по ключам мапы ссылок, если лист ссылок содержит адрес текущей страницы, добавляем значение ее пр в формулу
+                for (String link2 : linksMap.keySet()) {
+                    if (link2 != pageURL) {
+                        if (linksMap.get(link2).contains(pageURL)) {
+                            resultMap.put(pageURL, resultMap.get(pageURL) + (0.85 + (resultMap.get(link2) / linksMap.get(link2).size())));
+                        }
+                    }
+                }
+            }
+
+        }
+        System.out.println("Page ranks calculated");
+        return resultMap;
+    }
+
+    public void getPageRanksInFile (HashMap<String, Double> map) throws IOException{
+        File outputFile = new File("C:/Study/Idea_Projects/Crawler/src/main/resources/OutputFiles","PR.txt");
+        FileWriter writer = new FileWriter(outputFile);
+        for(String page:map.keySet()){
+            writer.write(page+map.get(page).toString()+"          "+"\n");
+        }
+        writer.flush();
+        writer.close();
+        System.out.println("Page ranks outputted.");
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Crawler MyCrawler = new Crawler(100);
         MyCrawler.doCrawl("https://sobakainfo.ru/porody/");
+        System.out.println("Crawling was finished succesfully");
         MyCrawler.returnResults("index.txt");
+        System.out.println("Results returned");
+        MyCrawler.getPageRanksInFile(MyCrawler.getPRMap());
 
     }
 
